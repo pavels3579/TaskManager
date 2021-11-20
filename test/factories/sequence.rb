3e1dbf161e2b54cbdc86314e9786663f52c961c7
Pavel Semenkin <pavels357@yandex.ru>
2021-11-20 23:42:58 +0400

    review fixes #6

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  sequence :string, aliases: [:first_name, :last_name, :password, :state] do |n|
+  sequence :string, aliases: [:first_name, :last_name, :password] do |n|
     "string#{n}"
   end
 
